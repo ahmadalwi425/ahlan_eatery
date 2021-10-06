b7b3c5ed4e7e59213cd632814a64883d8570f3c0
ahmadalwi425@gmail.com <ahmadalwi425@gmail.com>
2021-10-06 12:08:56 +0700

    base notification

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -127,6 +127,13 @@
 	<script src="{{asset('assets')}}/vendor/chart.js/dist/Chart.extension.js"></script>
 	<!-- Argon JS -->
 	<script src="{{asset('assets')}}/js/argon.js?v=1.2.0"></script>
+	<script src="{{asset('assets/js/push.min.js')}}"></script>
+	<script>
+		Push.create('Pesanan Baru',{
+			Body : "Halo dsfdsfdsfsdf",
+			timeout : 5000
+		});
+	</script>
 </body>
 
 </html>
