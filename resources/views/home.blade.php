9152a5dcd12deff470dfe1caafb3f0e1e137b01b
ahmadalwi425@gmail.com <ahmadalwi425@gmail.com>
2021-10-15 22:42:28 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,32 @@
 @extends('layouts.templateAuth')
 
 @section('content')
-<div class="row">
-    <div class="col-sm-12">
-        <!-- Authentication card start -->
-        <div class="login-card card-block auth-body mr-auto ml-auto">
-            <form class="md-float-material" role="form" action="{{ route('login') }}" method="POST">
-                @csrf
-                <div class="text-center">
-                    <img src="{{asset('assets')}}/images/logo.jpeg" alt="logo.png" style="height: 100px">
-                </div>
-                <div class="auth-box">
-                    <div class="row m-b-20">
-                        <div class="col-md-12">
-                            <h3 class="text-left txt-primary">Login</h3>
-                        </div>
-                    </div>
-                    <hr />
-                    <div class="input-group">
-                        <input type="text" class="form-control" name="name" placeholder="Enter your name">
-                        <span class="md-line"></span>
-                    </div>
-                    <div class="input-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                        <span class="md-line"></span>
-                    </div>
-                    <div class="row text-right">
-                        <div class="col-sm-7 col-xs-12">
-                            <a href="auth-reset-password.html" class="text-right f-w-600 text-inverse"> Forgot Your
-                                Password?</a>
-                        </div>
-                    </div>
-                    <div class="row m-t-30">
-                        <div class="col-md-12">
-                            <button type="submit"
-                                class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Sign in</button>
-                        </div>
-                    </div>
-                    <hr />
+<style>
 
-                </div>
-            </form>
-            <!-- end of form -->
-        </div>
-        <!-- Authentication card end -->
+
+#btnmenu{
+    display:flex;
+    align-items:center;
+    justify-content: center;
+    transition-duration:0.25s;
+    font-size:large;
+    background-color:#B26929;
+    opacity:0.8;
+}
+
+#btnmenu:hover{
+    background-color:black;
+    transition-duration:0.25s;
+}
+
+</style>
+<div class="row" style="height:500px;">
+    
+    <div class="col-sm-6" height="100%" id="btnmenu" onclick="window.open('{{url("scan")}}', '_blank').focus()">
+        <h1 class="ctbtn">Pesan Sekarang</h1>
+    </div>
+    <div class="col-sm-6" height="100%" id="btnmenu" onclick="window.open('{{url("login")}}', '_blank').focus()">
+        <h1 class="ctbtn">Login</h1>
     </div>
     <!-- end of col-sm-12 -->
 </div>
